f174b8c68a8f9b4efec02ee8c49098985ee99842
Oleh Hanyak <ohanyak@gmail.com>
2020-04-29 08:16:53 +0300

    add

---
+++ lesson_7/task2/src/index.js
@@ -0,0 +1,24 @@
+import { initTodoListHandlers } from './list/todoList';
+import { renderTasks } from './list/renderer';
+import { getTasksList } from './list/tasksGateway';
+import { setItem } from './list/storage';
+import './index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+    getTasksList()
+        .then(tasksList => {
+            setItem('tasksList', tasksList)
+            renderTasks();
+        });
+
+    initTodoListHandlers();
+});
+
+const onStorageChange = e => {
+    console.log(e)
+    if (e.key === 'tasksList') {
+        renderTasks();
+    }
+};
+
+window.addEventListener('storage', onStorageChange);
