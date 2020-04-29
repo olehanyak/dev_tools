0cdf2f219180f0dc2269081f9a2c14dbcf86f2c2
Oleh Hanyak <ohanyak@gmail.com>
2020-04-29 17:15:31 +0300

    add

---
+++ lesson_9/task2/src/list/tasksGateway.js
@@ -0,0 +1,28 @@
+const baseUrl = 'https://5e9e6e6ffb467500166c3f84.mockapi.io/api/v1/tasks';
+
+const mapTasks = (tasks) => tasks.map(({ _id, ...rest }) => ({ ...rest, id: _id }));
+
+
+export const getTasksList = () => fetch(baseUrl)
+  .then((response) => response.json())
+  .then((tasks) => mapTasks(tasks));
+
+export const createTask = (taskData) => fetch(baseUrl, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  body: JSON.stringify(taskData),
+});
+
+export const updateTask = (taskId, updatedTaskData) => fetch(`${baseUrl}/${taskId}`, {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  body: JSON.stringify(updatedTaskData),
+});
+
+export const deleteTask = (taskId) => fetch(`${baseUrl}/${taskId}`, {
+  method: 'DELETE',
+});
